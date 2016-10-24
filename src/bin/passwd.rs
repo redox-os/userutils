46631df4f09c39ff760b48d25249a48840ee20fa
Jeremy Soller <jackpot51@gmail.com>
2016-10-24 14:07:55 -0600

    Use OsRng to get random data for seed

--- src/bin/passwd.rs
+++ src/bin/passwd.rs
@@ -1,10 +1,11 @@
-extern crate termion;
+extern crate rand;
 extern crate userutils;
 
+use rand::{Rng, OsRng};
 use std::env;
 
 fn main() {
     let passwd = env::args().nth(1).unwrap();
-    let salt = env::args().nth(2).unwrap();
+    let salt = format!("{:X}", OsRng::new().unwrap().next_u64());
     println!("{}", userutils::Passwd::encode(&passwd, &salt));
 }
